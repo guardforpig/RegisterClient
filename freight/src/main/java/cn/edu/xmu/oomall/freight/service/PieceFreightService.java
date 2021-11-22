6608ea9317057af84f8447d77cc5296075086657
高 艺桐 <22920192204199@stu.xmu.edu.cn>
2021-11-22 10:40:24 +0000

    Update PieceFreightService.java

--- freight/src/main/java/cn/edu/xmu/oomall/freight/service/PieceFreightService.java
+++ freight/src/main/java/cn/edu/xmu/oomall/freight/service/PieceFreightService.java
@@ -3,8 +3,10 @@ package cn.edu.xmu.oomall.freight.service;
 import cn.edu.xmu.oomall.core.util.Common;
 import cn.edu.xmu.oomall.core.util.ReturnNo;
 import cn.edu.xmu.oomall.core.util.ReturnObject;
+import cn.edu.xmu.oomall.freight.dao.FreightModelDao;
 import cn.edu.xmu.oomall.freight.dao.PieceFreightDao;
-import cn.edu.xmu.oomall.freight.model.bo.PieceFreightBo;
+import cn.edu.xmu.oomall.freight.model.bo.FreightModel;
+import cn.edu.xmu.oomall.freight.model.bo.PieceFreight;
 import cn.edu.xmu.oomall.freight.model.po.FreightModelPo;
 import cn.edu.xmu.oomall.freight.model.vo.PieceFreightRetVo;
 import cn.edu.xmu.oomall.freight.model.vo.PieceFreightVo;
@@ -15,7 +17,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
-
 /**
  * @author Yitong  Gao
  */
@@ -23,64 +24,56 @@ import java.util.List;
 public class PieceFreightService {
     @Autowired
     private PieceFreightDao pieceFreightDao;
-    private final Byte PieceFreightType=1;
+    @Autowired
+    private FreightModelDao freightModelDao;
+    private static final Byte TYPE_WEIGHT=0;
+    private static final Byte TYPE_PIECE=1;
+
     /**
      * 新增件数运费模板
-     * @param createName
-     * @param createId
-     * @param id
-     * @param pieceFreightVo
+     * @param userId
+     * @param userName
+     * @param pieceFreight
      * @return
      */
     @Transactional(rollbackFor = Exception.class)
-    public ReturnObject addPieceFreight(String createName, Long createId, Long id, PieceFreightVo pieceFreightVo) {
-        PieceFreightBo pieceFreightBo = (PieceFreightBo) Common.cloneVo(pieceFreightVo, PieceFreightBo.class);
-        pieceFreightBo.setFreightModelId(id);
-        ReturnObject judgeRegionId=pieceFreightDao.judgeRegionId(pieceFreightBo.getRegionId());
-        if(!(boolean)judgeRegionId.getData())
+    public ReturnObject addPieceFreight( String userName,Long userId, PieceFreight pieceFreight) {
+        FreightModel freightModel=(FreightModel)freightModelDao.selectFreightModelById(pieceFreight.getFreightModelId()).getData();
+        if(freightModel==null)
         {
-            return new ReturnObject(ReturnNo.FREIGHT_REGIONEXIST,"运费模板中该地区已经定义");
+            return new ReturnObject(ReturnNo.RESOURCE_ID_NOTEXIST);
+        }
+        if(!freightModel.getType().equals(TYPE_PIECE))
+        {
+            return new ReturnObject(ReturnNo.FREIGHT_WRONGTYPE);
 
         }
-        ReturnObject returnObject1=pieceFreightDao.getFreightModelById(pieceFreightBo.getFreightModelId());
-        FreightModelPo freightModelPo=(FreightModelPo)returnObject1.getData();
-        if(freightModelPo==null)
-        {
-            return new ReturnObject(ReturnNo.RESOURCE_ID_NOTEXIST,"操作的资源id不存在");
-        }
-        if(!freightModelPo.getType().equals(PieceFreightType))
-        {
-            return new ReturnObject(ReturnNo.FREIGHT_WRONGTYPE,"该运费模板类型与内容不符");
-
-        }
-
-        ReturnObject returnObject = pieceFreightDao.addPieceFreight(pieceFreightBo, createId, createName);
-        if (returnObject.getData() == null) {
-            return returnObject;
-        }
-        PieceFreightBo pieceFreightBo1 = (PieceFreightBo) returnObject.getData();
-        PieceFreightRetVo pieceFreightRetVo = (PieceFreightRetVo) Common.cloneVo(pieceFreightBo1, PieceFreightRetVo.class);
+        PieceFreight returnPieceFreight = (PieceFreight) pieceFreightDao.addPieceFreight(pieceFreight,userId, userName).getData();
+        PieceFreightRetVo pieceFreightRetVo = (PieceFreightRetVo) Common.cloneVo(returnPieceFreight, PieceFreightRetVo.class);
         return new ReturnObject(pieceFreightRetVo);
     }
 
     /**
      * 店家或管理员查询件数运费模板的明细
-     *
-     * @param id
+     * @param freightModelId
      * @param page
      * @param pageSize
      * @return
      */
-    @Transactional(readOnly = true)
-    public ReturnObject getPieceFreight(Long id, Integer page, Integer pageSize) {
-        ReturnObject<PageInfo<PieceFreightBo>> pageInfoReturnObject = pieceFreightDao.getPieceFreight(id, page, pageSize);
+    @Transactional(rollbackFor=Exception.class, readOnly = true)
+    public ReturnObject getPieceFreight(Long freightModelId, Integer page, Integer pageSize) {
+        FreightModel freightModel = (FreightModel) freightModelDao.selectFreightModelById(freightModelId).getData();
+        if (freightModel==null) {
+            return new ReturnObject(ReturnNo.RESOURCE_ID_NOTEXIST);
+        }
+        ReturnObject<PageInfo<PieceFreight>> pageInfoReturnObject = pieceFreightDao.getPieceFreight(freightModelId, page, pageSize);
         if (pageInfoReturnObject.getData() == null) {
             return pageInfoReturnObject;
         }
-        PageInfo<PieceFreightBo> pieceFreightBoPageInfo = pageInfoReturnObject.getData();
+        PageInfo<PieceFreight> pieceFreightBoPageInfo = pageInfoReturnObject.getData();
         List<PieceFreightRetVo> retList = new ArrayList<>();
-        for (PieceFreightBo pieceFreightBo : pieceFreightBoPageInfo.getList()) {
-            PieceFreightRetVo pieceFreightRetVo = (PieceFreightRetVo) Common.cloneVo(pieceFreightBo, PieceFreightRetVo.class);
+        for (PieceFreight pieceFreight : pieceFreightBoPageInfo.getList()) {
+            PieceFreightRetVo pieceFreightRetVo = (PieceFreightRetVo) Common.cloneVo(pieceFreight, PieceFreightRetVo.class);
             retList.add(pieceFreightRetVo);
         }
         PageInfo<PieceFreightRetVo> p = (PageInfo<PieceFreightRetVo>) Common.cloneVo(pieceFreightBoPageInfo, new PageInfo<PieceFreightRetVo>().getClass());
@@ -97,7 +90,6 @@ public class PieceFreightService {
      */
     @Transactional(rollbackFor = Exception.class)
     public ReturnObject deletePieceFreight(Long id) {
-
         ReturnObject returnObject = pieceFreightDao.deletePieceFreight(id);
         return returnObject;
     }
@@ -111,16 +103,10 @@ public class PieceFreightService {
      * @return
      */
     @Transactional(rollbackFor = Exception.class)
-    public ReturnObject modifyPieceFreight(PieceFreightVo pieceFreightVo, Long id, Long userId, String userName) {
-        PieceFreightBo pieceFreightBo = (PieceFreightBo) Common.cloneVo(pieceFreightVo, PieceFreightBo.class);
-        pieceFreightBo.setId(id);
-        ReturnObject judgeRegionId=pieceFreightDao.judgeRegionId(pieceFreightBo.getRegionId());
-        if(!(boolean)judgeRegionId.getData())
-        {
-            return new ReturnObject(ReturnNo.FREIGHT_REGIONSAME,"运费模板中该地区已经定义");
-
-        }
-        ReturnObject returnObject = pieceFreightDao.modifyPieceFreight(pieceFreightBo, userId, userName);
+    public ReturnObject updatePieceFreight(PieceFreightVo pieceFreightVo, Long id, Long userId, String userName) {
+        PieceFreight pieceFreight = (PieceFreight) Common.cloneVo(pieceFreightVo, PieceFreight.class);
+        pieceFreight.setId(id);
+        ReturnObject returnObject = pieceFreightDao.updatePieceFreight(pieceFreight, userId, userName);
         return returnObject;
 
     }
