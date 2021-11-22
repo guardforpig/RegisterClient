e23a444a3f55c8139b4375e67fa8ee7789babed7
Ming Qiu <mingqiu@xmu.edu.cn>
2021-11-22 19:48:34 +0800

    修改Common为Util

---
+++ core/src/main/java/cn/edu/xmu/oomall/core/util/Util.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright School of Informatics Xiamen University
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+package cn.edu.xmu.oomall.core.util;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class Util {
+    /**
+     * 根据 errCode 修饰 API 返回对象的 HTTP Status
+     * @param returnObject 原返回 Object
+     * @return 修饰后的返回 Object
+     */
+    public static Object decorateReturnObject(ReturnObject returnObject) {
+        switch (returnObject.getCode()) {
+            case RESOURCE_ID_NOTEXIST:
+                // 404：资源不存在
+                return new ResponseEntity(
+                        ResponseUtil.fail(returnObject.getCode(), returnObject.getErrmsg()),
+                        HttpStatus.NOT_FOUND);
+
+            case AUTH_INVALID_JWT:
+            case AUTH_JWT_EXPIRED:
+                // 401
+                return new ResponseEntity(
+                        ResponseUtil.fail(returnObject.getCode(), returnObject.getErrmsg()),
+                        HttpStatus.UNAUTHORIZED);
+
+            case INTERNAL_SERVER_ERR:
+                // 500：数据库或其他严重错误
+                return new ResponseEntity(
+                        ResponseUtil.fail(returnObject.getCode(), returnObject.getErrmsg()),
+                        HttpStatus.INTERNAL_SERVER_ERROR);
+
+            case FIELD_NOTVALID:
+            case RESOURCE_FALSIFY:
+            case IMG_FORMAT_ERROR:
+            case IMG_SIZE_EXCEED:
+            case LATE_BEGINTIME:
+            case ACT_LATE_PAYTIME:
+            case ACT_EARLY_PAYTIME:
+            case COUPON_LATE_COUPONTIME:
+                // 400
+                return new ResponseEntity(
+                        ResponseUtil.fail(returnObject.getCode(), returnObject.getErrmsg()),
+                        HttpStatus.BAD_REQUEST);
+
+            case RESOURCE_ID_OUTSCOPE:
+            case  FILE_NO_WRITE_PERMISSION:
+                // 403
+                return new ResponseEntity(
+                        ResponseUtil.fail(returnObject.getCode(), returnObject.getErrmsg()),
+                        HttpStatus.FORBIDDEN);
+
+            case OK:
+                // 200: 无错误
+                Object data = returnObject.getData();
+                if (data != null){
+                    return ResponseUtil.ok(data);
+                }else{
+                    return ResponseUtil.ok();
+                }
+
+            default:
+                return ResponseUtil.fail(returnObject.getCode(), returnObject.getErrmsg());
+        }
+    }
+
+}
