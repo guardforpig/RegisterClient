51686b58ffb0725972927f59c1fb16351401cb78
高 艺桐 <22920192204199@stu.xmu.edu.cn>
2021-11-23 01:34:15 +0000

    Update testdata.sql

--- freight/testdata.sql
+++ freight/testdata.sql
@@ -3,3 +3,14 @@ LOCK TABLES `oomall_weight_freight` WRITE;
 INSERT INTO `oomall_weight_freight` VALUES (2,1,500,1000,100,50,10,5,0,152,1,'admin',NULL,NULL,'2021-11-17 9:18:26',NULL);
 /*!40000 ALTER TABLE `oomall_weight_freight` ENABLE KEYS */;
 UNLOCK TABLES;
+LOCK TABLES `oomall_piece_freight` WRITE;
+/*!40000 ALTER TABLE `oomall_piece_freight` DISABLE KEYS */;
+INSERT INTO `oomall_piece_freight` VALUES (5,2,1,10,1,5,5,1,'admin',NULL,NULL,'2021-11-22 14:47:21',NULL);
+/*!40000 ALTER TABLE `oomall_piece_freight` ENABLE KEYS */;
+UNLOCK TABLES;
+
+LOCK TABLES `oomall_piece_freight` WRITE;
+/*!40000 ALTER TABLE `oomall_piece_freight` DISABLE KEYS */;
+INSERT INTO `oomall_piece_freight` VALUES (6,2,1,10,1,5,6,1,'admin',NULL,NULL,'2021-11-22 14:47:21',NULL);
+/*!40000 ALTER TABLE `oomall_piece_freight` ENABLE KEYS */;
+UNLOCK TABLES;
