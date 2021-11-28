154c2736b8ad4c25ba3ec9c1c70d8c5b70301680
郑武帝 <582156915@qq.com>
2021-11-28 16:25:26 +0800

    取消注释

--- activity/src/main/java/cn/edu/xmu/oomall/activity/controller/AdvanceSaleController.java
+++ activity/src/main/java/cn/edu/xmu/oomall/activity/controller/AdvanceSaleController.java
@@ -38,67 +38,67 @@ public class AdvanceSaleController {
     GoodsService goodsService;
     @Autowired
     private HttpServletResponse httpServletResponse;
-//    /**
-//     * @author Gxc 22920192204194
-//     */
-//    @Audit
-//    @ApiOperation(value = "商铺管理员上线预售活动")
-//    @ApiImplicitParams({
-//            @ApiImplicitParam(paramType = "header", dataType = "String", name = "token", value = "用户的token", required = true),
-//            @ApiImplicitParam(paramType = "path", dataType = "Long", name = "shopId", value = "商铺id", required = true),
-//            @ApiImplicitParam(paramType = "path", dataType = "Long", name = "id", value = "预售活动id", required = true)
-//    })
-//    @PutMapping("/shops/{shopId}/advancesales/{id}/online")
-//    public Object onlineAdvancesale(Long adminId,String adminName, @PathVariable("shopId")Long shopId, @PathVariable("id")Long advancesaleId) {
-//        adminId=1L;adminName="店铺管理员";
-//        ReturnObject returnObject = null;
-//        returnObject= advanceSaleService.onlineAdvancesale(adminId,adminName,shopId,advancesaleId);
-//        return Common.decorateReturnObject(returnObject);
-//    }
-//    @Audit
-//    @ApiOperation(value = "商铺管理员下线预售活动")
-//    @ApiImplicitParams({
-//            @ApiImplicitParam(paramType = "header", dataType = "String", name = "token", value = "用户的token", required = true),
-//            @ApiImplicitParam(paramType = "path", dataType = "Long", name = "shopId", value = "商铺id", required = true),
-//            @ApiImplicitParam(paramType = "path", dataType = "Long", name = "id", value = "预售活动id", required = true)
-//    })
-//    @PutMapping("/shops/{shopId}/advancesales/{id}/offline")
-//    public Object offlineAdvancesale(Long adminId,String adminName, @PathVariable("shopId")Long shopId, @PathVariable("id")Long advancesaleId)  {
-//        adminId=1L;adminName="店铺管理员";
-//        ReturnObject returnObject = null;
-//        returnObject= advanceSaleService.offlineAdvancesale(adminId,adminName,shopId,advancesaleId);
-//        return Common.decorateReturnObject(returnObject);
-//    }
-//    @Audit
-//    @ApiOperation(value = "商铺管理员修改预售活动")
-//    @ApiImplicitParams({
-//            @ApiImplicitParam(paramType = "header", dataType = "String", name = "token", value = "用户的token", required = true),
-//            @ApiImplicitParam(paramType = "path", dataType = "Long", name = "shopId", value = "商铺id", required = true),
-//            @ApiImplicitParam(paramType = "path", dataType = "Long", name = "id", value = "预售活动id", required = true),
-//            @ApiImplicitParam(paramType = "body", dataType = "AdvanceSaleModifyVo", name = "advancesalemodifyvo", value = "修改内容", required = true)
-//    })
-//    @PutMapping("/shops/{shopId}/advancesales/{id}")
-//    public Object modifyAdvancesale(Long adminId, String adminName, @PathVariable("shopId")Long shopId, @PathVariable("id")Long advancesaleId,
-//                                    @RequestBody AdvanceSaleModifyVo advanceSaleModifyVo){
-//        adminId=1L;adminName="店铺管理员";
-//        ReturnObject returnObject=null;
-//        returnObject= advanceSaleService.modifyAdvancesale(adminId,shopId,adminName,advancesaleId,advanceSaleModifyVo);
-//        return Common.decorateReturnObject(returnObject);
-//    }
-//    @Audit
-//    @ApiOperation(value = "商铺管理员删除预售活动")
-//    @ApiImplicitParams({
-//            @ApiImplicitParam(paramType = "header", dataType = "String", name = "token", value = "用户的token", required = true),
-//            @ApiImplicitParam(paramType = "path", dataType = "Long", name = "shopId", value = "商铺id", required = true),
-//            @ApiImplicitParam(paramType = "path", dataType = "Long", name = "id", value = "预售活动id", required = true)
-//    })
-//    @DeleteMapping("/shops/{shopId}/advancesales/{id}")
-//    public Object deleteAdvancesale(Long adminId,String adminName, @PathVariable("shopId")Long shopId, @PathVariable("id")Long advancesaleId) {
-//        adminId=1L;adminName="店铺管理员";
-//        ReturnObject returnObject = null;
-//        returnObject= advanceSaleService.deleteAdvancesale(adminId,shopId,advancesaleId);
-//        return Common.decorateReturnObject(returnObject);
-//    }
+    /**
+     * @author Gxc 22920192204194
+     */
+    @Audit
+    @ApiOperation(value = "商铺管理员上线预售活动")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "header", dataType = "String", name = "token", value = "用户的token", required = true),
+            @ApiImplicitParam(paramType = "path", dataType = "Long", name = "shopId", value = "商铺id", required = true),
+            @ApiImplicitParam(paramType = "path", dataType = "Long", name = "id", value = "预售活动id", required = true)
+    })
+    @PutMapping("/shops/{shopId}/advancesales/{id}/online")
+    public Object onlineAdvancesale(Long adminId,String adminName, @PathVariable("shopId")Long shopId, @PathVariable("id")Long advancesaleId) {
+        adminId=1L;adminName="店铺管理员";
+        ReturnObject returnObject = null;
+        returnObject= advanceSaleService.onlineAdvancesale(adminId,adminName,shopId,advancesaleId);
+        return Common.decorateReturnObject(returnObject);
+    }
+    @Audit
+    @ApiOperation(value = "商铺管理员下线预售活动")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "header", dataType = "String", name = "token", value = "用户的token", required = true),
+            @ApiImplicitParam(paramType = "path", dataType = "Long", name = "shopId", value = "商铺id", required = true),
+            @ApiImplicitParam(paramType = "path", dataType = "Long", name = "id", value = "预售活动id", required = true)
+    })
+    @PutMapping("/shops/{shopId}/advancesales/{id}/offline")
+    public Object offlineAdvancesale(Long adminId,String adminName, @PathVariable("shopId")Long shopId, @PathVariable("id")Long advancesaleId)  {
+        adminId=1L;adminName="店铺管理员";
+        ReturnObject returnObject = null;
+        returnObject= advanceSaleService.offlineAdvancesale(adminId,adminName,shopId,advancesaleId);
+        return Common.decorateReturnObject(returnObject);
+    }
+    @Audit
+    @ApiOperation(value = "商铺管理员修改预售活动")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "header", dataType = "String", name = "token", value = "用户的token", required = true),
+            @ApiImplicitParam(paramType = "path", dataType = "Long", name = "shopId", value = "商铺id", required = true),
+            @ApiImplicitParam(paramType = "path", dataType = "Long", name = "id", value = "预售活动id", required = true),
+            @ApiImplicitParam(paramType = "body", dataType = "AdvanceSaleModifyVo", name = "advancesalemodifyvo", value = "修改内容", required = true)
+    })
+    @PutMapping("/shops/{shopId}/advancesales/{id}")
+    public Object modifyAdvancesale(Long adminId, String adminName, @PathVariable("shopId")Long shopId, @PathVariable("id")Long advancesaleId,
+                                    @RequestBody AdvanceSaleModifyVo advanceSaleModifyVo){
+        adminId=1L;adminName="店铺管理员";
+        ReturnObject returnObject=null;
+        returnObject= advanceSaleService.modifyAdvancesale(adminId,shopId,adminName,advancesaleId,advanceSaleModifyVo);
+        return Common.decorateReturnObject(returnObject);
+    }
+    @Audit
+    @ApiOperation(value = "商铺管理员删除预售活动")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "header", dataType = "String", name = "token", value = "用户的token", required = true),
+            @ApiImplicitParam(paramType = "path", dataType = "Long", name = "shopId", value = "商铺id", required = true),
+            @ApiImplicitParam(paramType = "path", dataType = "Long", name = "id", value = "预售活动id", required = true)
+    })
+    @DeleteMapping("/shops/{shopId}/advancesales/{id}")
+    public Object deleteAdvancesale(Long adminId,String adminName, @PathVariable("shopId")Long shopId, @PathVariable("id")Long advancesaleId) {
+        adminId=1L;adminName="店铺管理员";
+        ReturnObject returnObject = null;
+        returnObject= advanceSaleService.deleteAdvancesale(adminId,shopId,advancesaleId);
+        return Common.decorateReturnObject(returnObject);
+    }
 
     /**
      * @author Jiawei Zheng
