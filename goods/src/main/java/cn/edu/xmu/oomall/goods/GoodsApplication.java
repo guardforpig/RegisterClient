742cf89e77a5f6ca2123f88aee1103d825f04803
御姐 <162095214@qq.com>
2021-12-01 13:09:51 +0800

    已审核，goods模块错误修改

--- goods/src/main/java/cn/edu/xmu/oomall/goods/GoodsApplication.java
+++ goods/src/main/java/cn/edu/xmu/oomall/goods/GoodsApplication.java
@@ -1,11 +1,11 @@
 package cn.edu.xmu.oomall.goods;
 
 import cn.edu.xmu.privilegegateway.annotation.aop.PageAspect;
-import cn.edu.xmu.privilegegateway.aop.PageAspect;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
@@ -17,9 +17,11 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication(scanBasePackages = {"cn.edu.xmu.oomall.core", "cn.edu.xmu.oomall.goods","cn.edu.xmu.privilegegateway"})
 @EnableConfigurationProperties
 @MapperScan("cn.edu.xmu.oomall.goods.mapper")
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = "cn.edu.xmu.oomall.goods.microservice")
 public class GoodsApplication {
 
-    
+
 
     public static void main(String[] args) {
 
