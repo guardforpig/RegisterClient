2afabe0d189c60cbfd1d533ba4383747a1ce3fcb
GZYWfff <2546088655@qq.com>
2021-12-01 13:54:53 +0800

    wechatpay-vo

---
+++ wechatpay/src/main/java/cn/edu/xmu/oomall/wechatpay/model/vo/WeChatPayPrepayRetVo.java
@@ -0,0 +1,18 @@
+package cn.edu.xmu.oomall.wechatpay.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author ziyi guo
+ * @date 2021/11/30
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class WeChatPayPrepayRetVo {
+
+    private String prepayId;
+
+}
---
+++ wechatpay/src/main/java/cn/edu/xmu/oomall/wechatpay/model/vo/WeChatPayRefundRetVo.java
@@ -0,0 +1,63 @@
+package cn.edu.xmu.oomall.wechatpay.model.vo;
+
+import cn.edu.xmu.oomall.wechatpay.model.bo.WeChatPayRefund;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author ziyi guo
+ * @date 2021/11/30
+ */
+@Data
+@NoArgsConstructor
+public class WeChatPayRefundRetVo {
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public class RefundAmountRetVo{
+        private Integer total;
+        private Integer refund;
+        private Integer payerTotal;
+        private Integer payerRefund;
+        private Integer settlementRefund;
+        private Integer settlementTotal;
+        private Integer discountRefund;
+        private String currency;
+    }
+
+    private String refundId;
+
+    private String outRefundNo;
+
+    private String transactionId;
+
+    private String outTradeNo;
+
+    private String channel;
+
+    private String userReceivedAccount;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern ="yyyy-MM-dd'T'HH:mm:ss.SSS" ,timezone = "GMT+8")
+    private LocalDateTime createTime;
+
+    private String status;
+
+    private RefundAmountRetVo amount;
+
+    public WeChatPayRefundRetVo(WeChatPayRefund weChatPayRefund){
+        this.refundId = weChatPayRefund.getRefundId();
+        this.outRefundNo = weChatPayRefund.getOutRefundNo();
+        this.transactionId = weChatPayRefund.getTransactionId();
+        this.outTradeNo = weChatPayRefund.getOutTradeNo();
+        this.channel = weChatPayRefund.getChannel();
+        this.userReceivedAccount = weChatPayRefund.getUserReceivedAccount();
+        this.createTime = weChatPayRefund.getCreateTime();
+        this.status = weChatPayRefund.getStatus();
+        this.amount = new RefundAmountRetVo(weChatPayRefund.getTotal(), weChatPayRefund.getRefund(), weChatPayRefund.getPayerTotal(), weChatPayRefund.getPayerRefund(), weChatPayRefund.getSettlementRefund(), weChatPayRefund.getSettlementTotal(), weChatPayRefund.getDiscountRefund(), weChatPayRefund.getCurrency());
+    }
+}
