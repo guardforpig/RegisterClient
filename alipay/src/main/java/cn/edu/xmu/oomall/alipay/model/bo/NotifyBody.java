23201d82438b39881ea3715c3f97eaf5470224bf
mawangdan <mawangdan@foxmail.com>
2021-12-02 14:57:13 +0800

    3-4 NotifyBody设置默认值，更改时间格式

--- alipay/src/main/java/cn/edu/xmu/oomall/alipay/model/bo/NotifyBody.java
+++ alipay/src/main/java/cn/edu/xmu/oomall/alipay/model/bo/NotifyBody.java
@@ -13,7 +13,7 @@ public class NotifyBody {
     /**
      * 回调时间
      */
-    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern ="yyyy-MM-dd HH:mm:ss.SSS" ,timezone = "GMT+8")
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern ="yyyy-MM-dd HH:mm:ss" ,timezone = "GMT+8")
     private LocalDateTime notify_time;
 
     /**
@@ -41,35 +41,35 @@ public class NotifyBody {
     /**
      * 通知的类型固定:trade_status_sync
      */
-    private String notify_type;
+    private String notify_type="trade_status_sync";
     /**
      * 通知校验 ID 固定：ac05099524730693a8b330c5ecf72da9786
      */
-    private String notify_id;
+    private String notify_id="ac05099524730693a8b330c5ecf72da9786";
     /**
      * 支付宝分配给开发者的应用 ID。固定:20214072300007148
      */
-    private String app_id;
+    private String app_id="20214072300007148";
     /**
      * 编码格式，如 utf-8、gbk、gb2312 等。固定:utf-8
      */
-    private String charset;
+    private String charset="utf-8";
     /**
      * 版本：固定：1.0
      */
-    private String version;
+    private String version="1.0";
     /**
      * 固定：RSA2
      */
-    private  String sign_type;
+    private  String sign_type="RSA2";
     /**
      * 固定:601510b7970e52cc63db0f4497cf70e
      */
-    private String sign;
+    private String sign="601510b7970e52cc63db0f4497cf70e";
     /**
      * 固定:2013112011001004330000121536
      */
-    private String trade_no;
+    private String trade_no="2013112011001004330000121536";
 
 
     public NotifyBody(LocalDateTime notify_time, String out_trade_no, String trade_status, String out_biz_no) {
