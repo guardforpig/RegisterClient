6c469331d3bec8004498dd03299ce489213f9ff3
郑武帝 <582156915@qq.com>
2021-12-07 10:07:18 +0800

    修改引用的Common位置

--- activity/src/main/java/cn/edu/xmu/oomall/activity/dao/AdvanceSaleDao.java
+++ activity/src/main/java/cn/edu/xmu/oomall/activity/dao/AdvanceSaleDao.java
@@ -3,7 +3,7 @@ package cn.edu.xmu.oomall.activity.dao;
 import cn.edu.xmu.oomall.activity.mapper.AdvanceSalePoMapper;
 import cn.edu.xmu.oomall.activity.model.bo.AdvanceSale;
 import cn.edu.xmu.oomall.activity.model.po.AdvanceSalePo;
-import cn.edu.xmu.privilegegateway.annotation.util.Common;
+import cn.edu.xmu.oomall.core.util.Common;
 import cn.edu.xmu.privilegegateway.annotation.util.RedisUtil;
 import cn.edu.xmu.oomall.core.util.ReturnNo;
 import cn.edu.xmu.oomall.core.util.ReturnObject;
