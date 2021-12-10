3a8d407550a544f2efe9f8379c1bbd2eed2d124e
mingqcn <jiraxmu@163.com>
2021-12-10 14:26:43 +0800

    change scripts

--- restart.sh
+++ restart.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-curl 'http://172.16.4.1/webdav/deploy/$1.jar'  --output '..'  --user ooad_javaee:$2
+curl 'http://172.16.4.1/webdav/deploy/$1.jar'  --output '$1.jar'  --user ooad_javaee:$2
 bash shutdown.sh $1
 bash startup.sh $1
 
