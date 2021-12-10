db53bdc78bdee6e2e6e6c406f7e6da7829eb2e1a
mingqcn <jiraxmu@163.com>
2021-12-10 13:28:42 +0800

    change scripts

--- restart.sh
+++ restart.sh
@@ -2,7 +2,7 @@
 
 JAR=$1-0.0.1-SNAPSHOT
 echo "$JAR"
-curl --user ooad_javaee:$2 -T http://172.16.4.1/webdav/deploy/${JAR}.jar ..
+wget --user ooad_javaee:$2 -T http://172.16.4.1/webdav/deploy/${JAR}.jar ..
 bash shutdown.sh ${JAR}
 bash startup.sh ${JAR}
 
