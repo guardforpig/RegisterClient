fd591a3b6eb8c3aeca4715c6f5949850e34d9f7e
mingqcn <jiraxmu@163.com>
2021-12-10 13:16:40 +0800

    change scripts

--- restart.sh
+++ restart.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-curl --user ooad_javaee:$2 -T http://172.16.4.1/webdav/deploy/$1-0.1.1-SNAPSHOT.jar ..
+curl --user ooad_javaee:$2 -T http://172.16.4.1/webdav/deploy/$1-0.0.1-SNAPSHOT.jar ..
 bash shutdown.sh $1
 bash startup.sh $1
 
--- shutdown.sh
+++ shutdown.sh
@@ -4,7 +4,7 @@ target_dir=`pwd`
 
 pid=`ps ax | grep -i $1 | grep ${target_dir} | grep java | grep -v grep | awk '{print $1}'`
 if [ -z "$pid" ] ; then
-        echo "No nacosServer running."
+        echo "No $1 Server running."
         exit -1;
 fi
 
