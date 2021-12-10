9e56f21b5f13a25d7df4b345717bab611303bd5a
mingqcn <jiraxmu@163.com>
2021-12-10 10:40:06 +0800

    dd main class

--- daily_test.sh
+++ daily_test.sh
@@ -25,7 +25,7 @@ echo '-------------------building comment-------------------------'
 cd /home/mingqiu/privilegegateway/privilegeservice
 git checkout pom.xml
 sed -i 's#'''$origin_dir'''#'''$daily_dir'''#g' pom.xml
-mvn clean test
+mvn clean package
 mvn site:site site:deploy
 
 cd /home/mingqiu/oomall
@@ -51,7 +51,7 @@ mvn site:site site:deploy
 echo '-------------------building comment-------------------------'
 cd /home/mingqiu/oomall/comment
 sed -i 's#'''$origin_dir'''#'''$daily_dir'''#g' pom.xml
-mvn clean test
+mvn clean package
 mvn site:site site:deploy
 
 echo '-------------------building feight-------------------------'
@@ -63,26 +63,29 @@ mvn site:site site:deploy
 echo '-------------------building shop-------------------------'
 cd /home/mingqiu/oomall/shop
 sed -i 's#'''$origin_dir'''#'''$daily_dir'''#g' pom.xml
-mvn clean test
+mvn clean package
 mvn site:site site:deploy
 
 echo '-------------------building goods-------------------------'
 cd /home/mingqiu/oomall/goods
 sed -i 's#'''$origin_dir'''#'''$daily_dir'''#g' pom.xml
-mvn clean test
+mvn clean package
 mvn site:site site:deploy
 
 echo '-------------------building coupon-------------------------'
 cd /home/mingqiu/oomall/coupon
 sed -i 's#'''$origin_dir'''#'''$daily_dir'''#g' pom.xml
-mvn clean test
+mvn clean package
 mvn site:site site:deploy
 
 echo '-------------------building activity-------------------------'
 cd /home/mingqiu/oomall/activity
 sed -i 's#'''$origin_dir'''#'''$daily_dir'''#g' pom.xml
-mvn clean test
+mvn clean package
 mvn site:site site:deploy
+
+curl --user ooad_javaee:12345678 -T /home/mingqiu/oomall/activity/target http://172.16.4.1/webdav/daily-report/$time/console.log
+
 
 
 curl --user ooad_javaee:12345678 -T /home/mingqiu/logs/daily_test.log http://172.16.4.1/webdav/daily-report/$time/console.log
