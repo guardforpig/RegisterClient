fba245691948245da39d0f0f21b2c86e355a674b
mingqcn <jiraxmu@163.com>
2021-12-10 14:24:14 +0800

    change scripts

--- restart.sh
+++ restart.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-wget --user ooad_javaee:$2 -T http://172.16.4.1/webdav/deploy/$1.jar ..
+curl 'http://172.16.4.1/webdav/deploy/$1.jar'  --output '..'  --user ooad_javaee:$2
 bash shutdown.sh $1
 bash startup.sh $1
 
