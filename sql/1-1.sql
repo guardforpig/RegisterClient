f7925c37138a55d2a961c1e7f8b7da7611315389
GYking <22920192204292@stu.xmu.edu.cn>
2021-12-12 17:28:35 +0800

    sql修改

---
+++ sql/1-1.sql
@@ -0,0 +1,26 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+--
+-- Host: 192.168.31.241    Database: oomall
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `oomall_advance_sale`
+--
+
+LOCK TABLES `oomall_product_draft` WRITE;
+/*!40000 ALTER TABLE `oomall_product_draft` DISABLE KEYS */;
+INSERT INTO `oomall_product_draft`(id,shop_id,product_id) VALUES (70,10,1576);
+/*!40000 ALTER TABLE `oomall_product_draft` ENABLE KEYS */;
+UNLOCK TABLES;
