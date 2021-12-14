31ed1de1d05ed9f4b17becaf13cf1a73f427c3cd
heyun <heyun@gmail.com>
2021-12-14 12:15:50 +0800

    fix(#20): 注入MockBean

--- goods/src/test/java/cn/edu/xmu/oomall/goods/controller/GoodsControllerTest.java
+++ goods/src/test/java/cn/edu/xmu/oomall/goods/controller/GoodsControllerTest.java
@@ -1,7 +1,10 @@
 package cn.edu.xmu.oomall.goods.controller;
 
+import cn.edu.xmu.oomall.core.util.ReturnNo;
+import cn.edu.xmu.oomall.core.util.ReturnObject;
 import cn.edu.xmu.oomall.goods.GoodsApplication;
 import cn.edu.xmu.oomall.goods.microservice.CategroyService;
+import cn.edu.xmu.oomall.goods.microservice.FreightService;
 import cn.edu.xmu.oomall.goods.microservice.ShopService;
 import cn.edu.xmu.oomall.goods.microservice.vo.CategoryVo;
 import cn.edu.xmu.oomall.goods.microservice.vo.SimpleCategoryVo;
@@ -60,7 +63,8 @@ class GoodsControllerTest {
     private RedisUtil redisUtil;
     @MockBean
     private CategroyService categroyService;
-
+    @MockBean
+    private FreightService freightService;
     @Test
     public void ListByfreightIdTest1() throws Exception
     {
@@ -929,15 +933,15 @@ class GoodsControllerTest {
     @Test
     @Transactional
     public void getFreightModels() throws Exception {
-        CustomComparator CUSTOM_COMPARATOR = new CustomComparator(JSONCompareMode.LENIENT,
-                new Customization("data.id", (o1, o2) -> true));
+        Mockito.when(freightService.getFreightModel(10L,1L)).thenReturn(new ReturnObject<>(ReturnNo.OK));
+
         adminToken = jwtHelper.createToken(1L, "admin", 0L, 3600, 0);
         String responseString = this.mockMvc.perform(get("/shops/10/products/1576/freightmodels").header("authorization", adminToken).contentType("application/json;charset=UTF-8"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("application/json;charset=UTF-8"))
                 .andReturn().getResponse().getContentAsString();
         String expected = "{\"errno\": 0,\"errmsg\": \"成功\"}";
-        JSONAssert.assertEquals(expected, responseString, CUSTOM_COMPARATOR);
+        JSONAssert.assertEquals(expected, responseString, true);
     }
 
     @Test
