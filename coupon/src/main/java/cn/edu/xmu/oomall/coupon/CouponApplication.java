88079e463819c92670aabdfe3f2563730c1195e5
蒋欣雨 <22920192204219@stu.xmu.edu.cn>
2021-12-15 14:51:40 +0000

    Update coupon/src/main/java/cn/edu/xmu/oomall/coupon/CouponApplication.java

--- coupon/src/main/java/cn/edu/xmu/oomall/coupon/CouponApplication.java
+++ coupon/src/main/java/cn/edu/xmu/oomall/coupon/CouponApplication.java
@@ -23,12 +23,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class CouponApplication {
 
     public static void main(String[] args) {
-        try {
             SpringApplication.run(CouponApplication.class, args);
-        }
-        catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
     }
 
 }
