d313dfd5e4d3a27a519a803f7fd9d3adb75103a3
Ming Qiu <mingqiu@xmu.edu.cn>
2021-12-17 23:15:52 +0800

    修改测试

--- public-test/src/test/java/cn/edu/xmu/oomall/privilege/LoginTest.java
+++ public-test/src/test/java/cn/edu/xmu/oomall/privilege/LoginTest.java
@@ -15,18 +15,15 @@ public class LoginTest extends BaseTestOomall {
 
     private static final String TESTURL ="/privilege/login";
 
-
     /**
      * 签名错误的用户
      * @throws Exception
      */
     @Test
     public void login1() throws Exception {
-        String requireJson = null;
-
-        requireJson = "{\"name\":\"wrong_sign\",\"password\":\"123456\"}";
+        String requireJson = "{\"name\":\"wrong_sign\",\"password\":\"123456\"}";
         this.gatewayClient.post().uri(TESTURL).bodyValue(requireJson).exchange()
-                .expectStatus().isCreated()
+                .expectStatus().isForbidden()
                 .expectBody()
                 .jsonPath("$.errno").isEqualTo(ReturnNo.RESOURCE_FALSIFY.getCode());
     }
@@ -34,18 +31,16 @@ public class LoginTest extends BaseTestOomall {
 
     /**
      * @author Song Runhan
+     *         //region 密码错误的用户登录
      * @date Created in 2020/11/4 16:00
      */
     @Test
     public void login2() throws Exception {
-        String requireJson = null;
-        WebTestClient.RequestHeadersSpec res = null;
-
-        //region 密码错误的用户登录
-        requireJson = "{\"name\":\"13088admin\",\"password\":\"000000\"}";
+        String requireJson = "{\"name\":\"13088admin\",\"password\":\"000000\"}";
         this.gatewayClient.post().uri(TESTURL)
                 .bodyValue(requireJson)
                 .exchange()
+                .expectStatus().isForbidden()
                 .expectHeader().contentType("application/json;charset=UTF-8")
                 .expectBody()
                 .jsonPath("$.errno").isEqualTo(ReturnNo.AUTH_INVALID_ACCOUNT.getCode());
@@ -57,14 +52,11 @@ public class LoginTest extends BaseTestOomall {
      */
     @Test
     public void login3() throws Exception {
-        String requireJson = null;
-
-        //region 用户名错误的用户登录
-        requireJson = "{\"name\":\"NotExist\",\"password\":\"123456\"}";
+        String requireJson = "{\"name\":\"NotExist\",\"password\":\"123456\"}";
         this.gatewayClient.post().uri(TESTURL)
                 .bodyValue(requireJson)
                 .exchange()
-                .expectStatus().isOk()
+                .expectStatus().isForbidden()
                 .expectHeader().contentType("application/json;charset=UTF-8")
                 .expectBody()
                 .jsonPath("$.errno").isEqualTo(ReturnNo.AUTH_INVALID_ACCOUNT.getCode());
@@ -91,15 +83,12 @@ public class LoginTest extends BaseTestOomall {
 
     /**
      * @author Song Runhan
+     * //region 没有输入密码（密码空）的用户登录
      * @date Created in 2020/11/4 16:00
      */
     @Test
     public void login5() throws Exception {
-        String requireJson = null;
-        WebTestClient.RequestHeadersSpec res = null;
-
-        //region 没有输入密码（密码空）的用户登录
-        requireJson = "{\"name\":\"537300010\",\"password\":\"\"}";
+        String requireJson = "{\"name\":\"537300010\",\"password\":\"\"}";
         this.gatewayClient.post().uri(TESTURL)
                 .bodyValue(requireJson)
                 .exchange().expectStatus().isBadRequest()
@@ -114,10 +103,7 @@ public class LoginTest extends BaseTestOomall {
      */
     @Test
     public void login6() throws Exception {
-        String requireJson = null;
-
-        //region 用户重复登录
-        requireJson = "{\"name\":\"13088admin\",\"password\":\"123456\"}";
+        String requireJson  = "{\"name\":\"13088admin\",\"password\":\"123456\"}";
         byte[] response = this.gatewayClient.post().uri(TESTURL)
                 .bodyValue(requireJson)
                 .exchange()
@@ -127,11 +113,13 @@ public class LoginTest extends BaseTestOomall {
                 .jsonPath("$.errno").isEqualTo(ReturnNo.OK.getCode())
                 .returnResult().getResponseBodyContent();
 
-        byte[] response1 = this.gatewayClient.post().uri(TESTURL).bodyValue(requireJson).exchange().expectStatus().isCreated()
+        byte[] response1 = this.gatewayClient.post().uri(TESTURL)
+                .bodyValue(requireJson)
+                .exchange()
+                .expectStatus().isCreated()
                 .expectHeader().contentType("application/json;charset=UTF-8")
                 .expectBody()
                 .jsonPath("$.errno").isEqualTo(ReturnNo.OK.getCode())
-                .jsonPath("$.errmsg").isEqualTo("成功")
                 .returnResult().getResponseBodyContent();
 
         String jwt = JacksonUtil.parseString(new String(response,"UTF-8"), "data");
@@ -146,10 +134,7 @@ public class LoginTest extends BaseTestOomall {
      */
     @Test
     public void login7() throws Exception {
-        String requireJson = null;
-        WebTestClient.RequestHeadersSpec res = null;
-        //region 当前状态不可登录的用户登录
-        requireJson = "{\"name\":\"5264500009\",\"password\":\"123456\"}";
+        String requireJson  = "{\"name\":\"5264500009\",\"password\":\"123456\"}";
         this.gatewayClient.post().uri(TESTURL)
                 .bodyValue(requireJson)
                 .exchange().expectStatus().isForbidden()
@@ -167,7 +152,7 @@ public class LoginTest extends BaseTestOomall {
         String requireJson =  "{\"name\":\"asdewetsa@tttt\",\"password\":\"123456\"}";
         this.gatewayClient.post().uri(TESTURL)
                 .bodyValue(requireJson)
-                .exchange().expectStatus().isOk()
+                .exchange().expectStatus().isCreated()
                 .expectHeader().contentType("application/json;charset=UTF-8")
                 .expectBody()
                 .jsonPath("$.errno").isEqualTo(ReturnNo.OK.getCode());
@@ -182,7 +167,7 @@ public class LoginTest extends BaseTestOomall {
         String requireJson =  "{\"name\":\"223477788\",\"password\":\"123456\"}";
         this.gatewayClient.post().uri(TESTURL)
                 .bodyValue(requireJson)
-                .exchange().expectStatus().isOk()
+                .exchange().expectStatus().isCreated()
                 .expectHeader().contentType("application/json;charset=UTF-8")
                 .expectBody()
                 .jsonPath("$.errno").isEqualTo(ReturnNo.OK.getCode());
