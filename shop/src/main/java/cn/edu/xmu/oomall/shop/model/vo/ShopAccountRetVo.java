b68f774103fdcfe366dc4b8b98d30c97e083205e
依古比古 <2282944711@qq.com>
2021-12-17 08:52:39 +0800

    修改时间格式，修改vo错误，修改测试代码

--- shop/src/main/java/cn/edu/xmu/oomall/shop/model/vo/ShopAccountRetVo.java
+++ shop/src/main/java/cn/edu/xmu/oomall/shop/model/vo/ShopAccountRetVo.java
@@ -1,28 +1,24 @@
 package cn.edu.xmu.oomall.shop.model.vo;
 
-import cn.edu.xmu.oomall.core.model.VoObject;
-import cn.edu.xmu.oomall.shop.model.po.ShopAccountPo;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.time.ZonedDateTime;
 
 /**
- * @author  Xusheng Wang
- * @date  2021-11-11
+ * @author Xusheng Wang
+ * @date 2021/12/17
  * @studentId 34520192201587
  */
-
 @Data
-@NoArgsConstructor
-public class ShopAccountVo{
+public class ShopAccountRetVo {
+
     @NotNull
-    private long id;
+    private Long  id;
 
     @ApiModelProperty(value = "支付渠道")
     private Byte type;
@@ -36,13 +32,10 @@ public class ShopAccountVo{
     @ApiModelProperty(value = "汇入优先级")
     private Byte priority;
 
-
     private SimpleAdminUserVo creator;
-    @DateTimeFormat(pattern = "uuuu-MM-dd'T'HH:mm:ss.SSSXXX")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "uuuu-MM-dd'T'HH:mm:ss.SSSXXX")
-    private LocalDateTime gmtCreate;
-    @DateTimeFormat(pattern = "uuuu-MM-dd'T'HH:mm:ss.SSSXXX")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "uuuu-MM-dd'T'HH:mm:ss.SSSXXX")
-    private LocalDateTime gmtModified;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "uuuu-MM-dd'T'HH:mm:ss.SSSZZZZZ")
+    private ZonedDateTime gmtCreate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "uuuu-MM-dd'T'HH:mm:ss.SSSZZZZZ")
+    private ZonedDateTime gmtModified;
     private SimpleAdminUserVo modifier;
 }
