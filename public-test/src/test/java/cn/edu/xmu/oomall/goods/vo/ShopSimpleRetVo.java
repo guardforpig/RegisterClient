36ddcf951c24dd9e976cdc93fbc128dd52fc9f8f
Ming Qiu <mingqiu@xmu.edu.cn>
2021-12-18 00:44:49 +0800

    增加商铺测试

---
+++ public-test/src/test/java/cn/edu/xmu/oomall/goods/vo/ShopSimpleRetVo.java
@@ -0,0 +1,11 @@
+package cn.edu.xmu.oomall.goods.vo;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class ShopSimpleRetVo {
+    private Long id;
+    private String name;
+}
