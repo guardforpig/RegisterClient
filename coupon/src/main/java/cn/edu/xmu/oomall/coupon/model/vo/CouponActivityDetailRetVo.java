be836b11aebe2c7d886f0ed541cb920f38b811af
Kakyoin <529106896@qq.com>
2021-12-23 11:17:31 +0800

    修改CouponActivityDetailRetVo

--- coupon/src/main/java/cn/edu/xmu/oomall/coupon/model/vo/CouponActivityDetailRetVo.java
+++ coupon/src/main/java/cn/edu/xmu/oomall/coupon/model/vo/CouponActivityDetailRetVo.java
@@ -1,10 +1,12 @@
 package cn.edu.xmu.oomall.coupon.model.vo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
 /**
  * @author:李智樑
@@ -16,14 +18,17 @@ import java.time.LocalDateTime;
 public class CouponActivityDetailRetVo {
     private Long id;
     private String name;
-    private LocalDateTime couponTime;
-    private LocalDateTime beginTime;
-    private LocalDateTime endTime;
-    private Long quantity;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ", timezone = "GMT+8")
+    private ZonedDateTime couponTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ", timezone = "GMT+8")
+    private ZonedDateTime beginTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ", timezone = "GMT+8")
+    private ZonedDateTime endTime;
+    private Integer quantity;
     private Byte quantityType;
     private Byte validTerm;
     private String imageUrl;
-    private Long numKey;
+    private Integer numKey;
     private String strategy;
     private Byte state;
 }
