4cac57ebbd325c269cf9f068ff59c30cb3c4fba4
limazhiluyao <limazhiluyao@gmail.com>
2021-12-24 10:24:37 +0800

    补充微信和支付宝

--- wechatpay/src/main/java/cn/edu/xmu/oomall/wechatpay/WeChatPayApplication.java
+++ wechatpay/src/main/java/cn/edu/xmu/oomall/wechatpay/WeChatPayApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication(scanBasePackages = {"cn.edu.xmu.oomall.core", "cn.edu.xmu.oomall.wechatpay", "cn.edu.xmu.privilegegateway"})
+@SpringBootApplication(scanBasePackages = {"cn.edu.xmu.oomall.core.util", "cn.edu.xmu.oomall.core.model","cn.edu.xmu.oomall.wechatpay", "cn.edu.xmu.privilegegateway"})
 @EnableConfigurationProperties
 @MapperScan("cn.edu.xmu.oomall.wechatpay.mapper")
 @EnableFeignClients(basePackages = "cn.edu.xmu.oomall.wechatpay.microservice")
